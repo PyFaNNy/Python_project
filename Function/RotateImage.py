f8757eb6f5bae1859a6aa0f5b385aa3e96a934e1
Gazuz33 <a4tonetak@gmail.com>
2022-05-30 00:53:26 +0300

    fix: fix imgPlacement and RotateImage functions

--- Function/RotateImage.py
+++ Function/RotateImage.py
@@ -1,7 +1,6 @@
-import cv2
+from PIL import Image
 import random
 def Rotate(img):
-    count = random.randint(0, 3);
-    for i in range(count):
-        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-    return img
+    deg = random.randint(1, 360);
+    img_rotate = img.rotate(deg, expand=True)
+    return img_rotate
--- Function/imgPlacement.py
+++ Function/imgPlacement.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-import cv2
-def Placement(pictures):
-    # creating an empty canvas
-    pic_box = plt.figure(figsize=(20, 20))
-    for i, picture in enumerate(pictures):
-    # convert BGR img to RGB
-    picture = cv2.cvtColor(picture, cv2.COLOR_BGR2RGB)
-    # add a cell to pic_box to display a current page
-    pic_box.add_subplot(1,N,i+1)
-    plt.imshow(picture)
-    # off axis
-    plt.axis('off')
-    plt.show()
+import os
+def Placement(pictures, N):
+    pictures = os.listdir('pic')
+    pictures = sorted(pictures)
+    background = plt.figure(figsize=(100, 100))
+    for i, picture in N:
+        (width, height) = picture.size
+        x = (100/N*i - width/2)
+        y = 50
+        background.paste(picture, (x, y))
+        i+=1
+        return background
