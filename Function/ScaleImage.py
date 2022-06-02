fb975bf32064273c4fa418704ee32df4f244b283
Haranovich Valiantsin <Sikuro12@yandex.by>
2022-06-02 14:56:57 +0300

    fix scale image

--- Function/ScaleImage.py
+++ Function/ScaleImage.py
@@ -3,5 +3,5 @@ import random
 
 
 def scale(img):
-    res = cv2.resize(img, None, fx=random.uniform(0.5, 2), fy=random.uniform(0.5, 2), interpolation=cv2.INTER_CUBIC)
+    res = cv2.resize(img, None, fx=random.uniform(0.5, 1.3), fy=random.uniform(0.5, 1.3), interpolation=cv2.INTER_CUBIC)
     return res
